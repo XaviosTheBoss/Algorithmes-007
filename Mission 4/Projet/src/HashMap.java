3e30f00713b9bf34bbc1af191c2223999ccaf623
XaviosTheBoss <perignonxa@hotmail.com>
2015-11-09 23:25:07 +0100

    construct, get, put

--- Mission 4/Projet/src/HashMap.java
+++ Mission 4/Projet/src/HashMap.java
@@ -1,45 +1,75 @@
 
 public class HashMap implements MapInterface<String, Integer> {
 
+    private int N;           /*nombre de paires de key-value dans la table*/
+    private int M;           /*taille de la table*/
+    private String[] keys;   /*les clés*/
+    private Integer[] vals;  /*les valeurs*/
+    
+    /*Initialise une table vide de taille du double de 'capacity'*/
+    public HashMap(int capacity) {
+        this.M = 2*capacity;
+        this.keys = (String[])new String[M];
+        this.vals = (Integer[])new Integer[M];
+    }
+    
 	@Override
 	public Integer get(String key) {
-		// TODO Auto-generated method stub
-		return null;
+		for (int i = hashCode(key); keys[i] != null; i = (i + 1) % M) 
+            if (keys[i].equals(key))
+                return vals[i];
+        return null;
 	}
-
+	
 	@Override
 	public Integer get(String key, int hashCode) {
-		// TODO Auto-generated method stub
-		return null;
+		for (int i = hashCode; keys[i] != null; i = (i + 1) % M) 
+            if (keys[i].equals(key))
+                return vals[i];
+        return null;
 	}
-
+	
 	@Override
 	public void put(String key, Integer value) {
-		// TODO Auto-generated method stub
-		
+        int i;
+        for (i = hashCode(key); keys[i] != null; i = (i + 1) % M) {
+            if (keys[i].equals(key)) {
+                vals[i] = value;
+                return;
+            }
+        }
+        keys[i] = key;
+        vals[i] = value;
+        N++;
 	}
-
+	
 	@Override
 	public void put(String key, Integer value, int hashCode) {
-		// TODO Auto-generated method stub
-		
+		int i;
+        for (i = hashCode; keys[i] != null; i = (i + 1) % M) {
+            if (keys[i].equals(key)) {
+                vals[i] = value;
+                return;
+            }
+        }
+        keys[i] = key;
+        vals[i] = value;
+        N++;
 	}
-
+	
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		return N;
 	}
-
+	
 	@Override
 	public int hashCode(String key) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
-
+	
 	@Override
-	public int incrementalHashCode(int keyLength, int lastKeyChar,
-			int lastHash, int lastChar) {
+	public int incrementalHashCode(int keyLength, int lastKeyChar, int lastHash, int lastChar) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
---
+++ Mission 4/Projet/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+	public static void main(String[] args) {
+    	MapInterface<String, Integer> map = new HashMap(3);
+    	map.put("coucou", 2);
+    	map.put("hihi", 4);
+    	map.put("lolo", 6);
+    	System.out.println("size : "+map.size());
+    	System.out.println("coucou : "+map.get("coucou"));
+    	System.out.println("hihi : "+map.get("hihi"));
+    	System.out.println("lolo : "+map.get("lolo"));
+    }
+}
