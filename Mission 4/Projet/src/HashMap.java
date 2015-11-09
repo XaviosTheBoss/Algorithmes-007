8bb1f469c103949ba7561e24e89718c8c9b594ab
XaviosTheBoss <perignonxa@hotmail.com>
2015-11-09 17:38:48 +0100

    hashmap

---
+++ Mission 4/Projet/src/HashMap.java
@@ -0,0 +1,47 @@
+
+public class HashMap implements MapInterface<String, Integer> {
+
+	@Override
+	public Integer get(String key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer get(String key, int hashCode) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void put(String key, Integer value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void put(String key, Integer value, int hashCode) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int hashCode(String key) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int incrementalHashCode(int keyLength, int lastKeyChar,
+			int lastHash, int lastChar) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
