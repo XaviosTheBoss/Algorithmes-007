f06fce1d5571ef488c8a60d963c60ed67e644bfb
XaviosTheBoss <perignonxa@hotmail.com>
2015-11-12 20:48:55 +0100

    classe plagiat créée

---
+++ Mission 4/Projet/src/Plagiarism.java
@@ -0,0 +1,13 @@
+import java.util.Map.Entry;
+import java.util.Set;
+
+
+public class Plagiarism implements PlagiarismInterface {
+
+	@Override
+	public Set<Entry<String, Integer>> detect(String doc) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
